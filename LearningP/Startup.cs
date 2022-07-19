6d6bf22861e0531f04e7a099922959138537900b
Gipnotyin <ruslangv49@gmail.com>
2022-07-19 19:50:35 +0300

    Я начал работу

--- LearningP/Startup.cs
+++ LearningP/Startup.cs
@@ -51,6 +51,7 @@ namespace LearningP
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                //I start 
             });
         }
     }
